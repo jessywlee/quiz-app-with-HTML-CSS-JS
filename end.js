c75120dbb939bdb9a98cb172577b6143bb1faa9f
Jessica Lee <to.jessywlee@gmail.com>
2021-02-24 23:29:08 +0900

    6. End Page with High Scores - JS

---
+++ end.js
@@ -0,0 +1,36 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+//because the value in local storage is a string
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+   saveScoreBtn.disabled = !username.value;
+   //if there is value, it enables.
+});
+
+
+saveHighScore = (e) => {
+    
+    e.preventDefault();
+    /* to prevent the default because forms by default will submit to a new page  */
+    const scoreBoard = {
+        score: Math.floor(Math.random()*100),
+        name: username.value
+    };
+
+    highScores.push(scoreBoard);
+    highScores.sort( (a,b) => b.score - a.score); // if b is higher than a, put b before a
+    highScores.splice(MAX_HIGH_SCORES); //to only keep top 5
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    console.log(highScores);
+    window.location.assign("index.html");
+
+};
